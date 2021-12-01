242e2281f90b714456db713336e92845f1c00bab
Artur Tryba <artur12304@wp.pl>
2021-12-01 21:58:55 +0100

    5kyu

---
+++ 13.CanYouGetTheLoop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+        //public static int getLoopSize(LoopDetector.Node startNode)
+        //{
+        //    var dict = new Dictionary<LoopDetector.Node, int>();
+        //    int index = 0;
+        //    while (true)
+        //    {
+        //        if (dict.ContainsKey(startNode))
+        //            return index - dict[startNode];
+        //        dict[startNode] = index++;
+        //        startNode = startNode.next;
+        //    }
+        //    return 0;
+        //}
+    }
+}
