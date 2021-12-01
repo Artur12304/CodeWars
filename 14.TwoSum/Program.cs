74a01ef4de125bd019426e8cf7d857ada52d7231
Artur Tryba <artur12304@wp.pl>
2021-12-01 21:58:59 +0100

    6kyu

---
+++ 14.TwoSum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3 };
+            int target = 4;
+            Console.WriteLine(String.Join(",", TwoSum(numbers, target)));
+            Console.ReadKey();
+        }
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+                for (int j = 0; j < numbers.Length; j++)
+                    if (i != j)
+                        if (numbers[i] + numbers[j] == target)
+                            return new int[] { i, j };
+            return new int[0];
+        }
+    }
+}
