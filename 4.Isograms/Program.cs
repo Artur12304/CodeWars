9d51d381aa1e73bcbdc4cc0b3c4f01a493774e98
Artur Tryba <artur12304@wp.pl>
2021-12-01 21:58:09 +0100

    7kyu

---
+++ 4.Isograms/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Dermatoglyphics";
+            Console.WriteLine(str.ToUpper().Distinct().Count() == str.Length);
+            Console.ReadLine();
+        }
+    }
+}
