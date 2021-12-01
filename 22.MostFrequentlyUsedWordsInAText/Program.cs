1e8cc16828e5aab1610b5c4cc0e63bc1cb5c9939
Artur Tryba <artur12304@wp.pl>
2021-12-01 21:59:46 +0100

    4kyu

---
+++ 22.MostFrequentlyUsedWordsInAText/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e";
+            var b = s.ToLower()
+                .Split(' ', '#', '\\', '/', '.', ',', ':', ';', '!', '-', '?', '_')
+                .GroupBy(x => x)
+                .Select(x => new KeyValuePair<string, int>(x.Key, x.Count()))
+                .OrderByDescending(x => x.Value)
+                .Select(x => x.Key)
+                .Where(x => x.Any(x => Char.IsLetter(x)))
+                .Take(3)
+                .ToList();
+            Console.WriteLine(String.Join(" ", b));
+
+            //var b = Regex.Matches(s.ToLowerInvariant(), @"('*[a-z]'*)+")
+            //.GroupBy(match => match.Value)
+            //.OrderByDescending(g => g.Count())
+            //.Select(p => p.Key)
+            //.Take(3)
+            //.ToList();
+            //Console.WriteLine(String.Join(" ", b));
+
+            Console.ReadKey();
+        }
+    }
+}
