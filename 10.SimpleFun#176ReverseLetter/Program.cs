4f19560e6510c9e06efbc1f63c80ca6c9ce22c71
Artur Tryba <artur12304@wp.pl>
2021-12-01 21:58:41 +0100

    7kyu

---
+++ 10.SimpleFun#176ReverseLetter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "k123rishan";
+            var b = str.Where(x => char.IsLetter(x)).Reverse();
+            Console.WriteLine(String.Join("", b));
+            Console.ReadKey();
+        }
+    }
+}
