08e76ee1ee6efbebf04b93dbf192cbbf3ab5036c
Artur Tryba <artur12304@wp.pl>
2021-12-01 21:59:36 +0100

    5kyu

---
+++ 20.SimplePigLatin/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Pig latin is cool !";
+            var s = String.Join(" ", str.Split(" ").Select(x => x.Length > 1 ? x.Substring(1) + x.Remove(1)+"ay" : x));
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
