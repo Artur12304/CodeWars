5b3859b40d50bf1015377b87e0dbee903ce587a3
Artur Tryba <artur12304@wp.pl>
2021-12-01 21:57:47 +0100

    6kyu

---
+++ 3.RomanNumeralsDecoder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> number = new List<int> { 1000, 500, 100, 50, 10, 5, 1 };
+            List<char> letter = new List<char> { 'M', 'D', 'C', 'L', 'X', 'V', 'I' };
+            string roman = "MMVIII";
+            int solution = 0;
+            for (int i = 0; i < roman.Length; i++)
+            {
+                if(roman.Length > i + 1 && letter.IndexOf(roman[i]) > letter.IndexOf(roman[i + 1]))
+                {
+                    solution += (number[letter.IndexOf(roman[i+1])] - number[letter.IndexOf(roman[i])]);
+                    i++;
+                }
+                else
+                {
+                    solution += number[letter.IndexOf(roman[i])];
+                }
+            }
+            Console.WriteLine(solution);
+            Console.ReadKey();
+        }
+    }
+}
