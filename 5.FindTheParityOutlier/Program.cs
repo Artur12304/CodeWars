668f6c4180f1d3d62b86be3b19cb7f3dcd2fb627
Artur Tryba <artur12304@wp.pl>
2021-12-01 21:58:16 +0100

    6kyu

---
+++ 5.FindTheParityOutlier/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = { 160, 3, 1719, 19, 11, 13, -21 };
+            Console.WriteLine(integers.Count(x => x % 2 != 0) == 1 ? integers.Where(x => x % 2 != 0).Sum() : integers.Where(x => x % 2 == 0).Sum());
+            Console.ReadKey();
+        }
+    }
+}
