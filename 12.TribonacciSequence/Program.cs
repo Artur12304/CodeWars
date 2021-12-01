82d38dd77e4a93800bc8cd8a52e6997182eab55f
Artur Tryba <artur12304@wp.pl>
2021-12-01 21:58:50 +0100

    6kyu

---
+++ 12.TribonacciSequence/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] signature = { 1, 1, 1 };
+            int n = 11;
+            for (int i = 3; i < n; i++)
+            {
+                signature = signature.Concat(new double[] { signature[i - 3] + signature[i - 2] + signature[i - 1] }).ToArray();
+            }
+            Console.WriteLine(String.Join(",", signature.Take(n).ToArray()));
+            Console.ReadKey();
+        }
+    }
+}
