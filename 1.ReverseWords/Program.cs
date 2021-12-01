e9ed0bfcec077e984ccf83d13e09a8432474d54a
Artur Tryba <artur12304@wp.pl>
2021-12-01 21:53:48 +0100

    7kyu

---
+++ 1.ReverseWords/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "This is an example!";
+
+            Console.WriteLine(string.Join(" ", str.Split(' ').Select(i => new string(i.Reverse().ToArray()))));
+            Console.ReadKey();
+        }
+    }
+ }
+
