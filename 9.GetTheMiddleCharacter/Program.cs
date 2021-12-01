23c71044da36ad0b59982ab956fe445a51e0498a
Artur Tryba <artur12304@wp.pl>
2021-12-01 21:58:35 +0100

    7kyu

---
+++ 9.GetTheMiddleCharacter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "middle";
+            Console.WriteLine(s.Length % 2 == 0 ? s.Substring(s.Length / 2 - 1, 2) : s.Substring(s.Length / 2, 1));
+            Console.ReadKey();
+        }
+    }
+}
