e92626cbbb92ccef9eacbade80c96352a41ffbf6
Artur Tryba <artur12304@wp.pl>
2021-12-01 21:58:31 +0100

    7kyu

---
+++ 8.RegexValidatePINCode/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pin = "12345";
+            bool a = pin.All(char.IsDigit) && (pin.Length==4 || pin.Length==6);
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
