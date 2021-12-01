f2c06ded10b5516391fc42eeacdf8f9b4654236c
Artur Tryba <artur12304@wp.pl>
2021-12-01 21:59:04 +0100

    5kyu

---
+++ 15.Scramblies/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "cedewaraaossoqqyt", str2 = "codewars";
+            Console.WriteLine(str2.All(x => str1.Count(y => y == x) >= str2.Count(y => y == x)));
+            Console.ReadKey();
+        }
+    }
+}
