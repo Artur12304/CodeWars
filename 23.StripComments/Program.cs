c674522dfb82896ca974d42d96fa8aae211d3141
Artur Tryba <artur12304@wp.pl>
2021-12-01 21:59:51 +0100

    4kyu

---
+++ 23.StripComments/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "apples, pears # and bananas\ngrapes\nbananas !apples";
+            var commentSymbols = new string[] { "#", "!" };
+            commentSymbols.ToList().ForEach(x => text = text.Replace(x, "#"));
+            var a = text.Split("\n").Select(x => x.Contains("#") ? x.Remove(x.IndexOf("#")).TrimEnd() : x.TrimEnd());
+            Console.WriteLine(String.Join("\n", a));
+            Console.ReadKey();
+        }
+    }
+}
