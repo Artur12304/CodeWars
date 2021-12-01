a1db3467563ae3a2a42deb6d1454368ed9cc8939
Artur Tryba <artur12304@wp.pl>
2021-12-01 21:59:18 +0100

    4kyu

---
+++ 18.Permutations/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(",", SinglePermutations("abc")));
+            Console.ReadKey();
+        }
+        private static List<string> SinglePermutations(string s)
+        {
+            var output = new List<string>();
+            if (s.Length == 1)
+            {
+                output.Add(s);
+            }
+            else
+            {
+                foreach (var c in s.Distinct())
+                {
+                    var tail = s.Remove(s.IndexOf(c), 1);
+                    foreach (var tailPerms in SinglePermutations(tail).Distinct())
+                    {
+                        output.Add(c + tailPerms);
+                    }
+                }
+            }
+            return output;
+        }
+    }
+}
