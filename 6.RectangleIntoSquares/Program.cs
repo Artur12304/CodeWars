46c8465fc888d71203a91a121915ff64de2ba42b
Artur Tryba <artur12304@wp.pl>
2021-12-01 21:58:21 +0100

    6kyu

---
+++ 6.RectangleIntoSquares/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> sqInRect = new List<int> {3,5};
+            List<int> solution = new List<int> { };
+            while (sqInRect[0] != 0 && sqInRect[1] != 0)
+            {
+                if(sqInRect[0] >= sqInRect[1])
+                {
+                    solution.Add(sqInRect[1]);
+                    sqInRect[0] -= sqInRect[1];
+                }
+                else
+                {
+                    solution.Add(sqInRect[0]);
+                    sqInRect[1] -= sqInRect[0];
+                }
+            }
+            foreach (var item in solution)
+            {
+                Console.Write(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
